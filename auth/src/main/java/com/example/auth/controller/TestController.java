bb9e20da0fe2f89ef6975ae507d10be3a97099d9
PigletHong <hongpiglet1995@gmail.com>
2024-09-18 17:40:53 +0900

    first commit

---
+++ auth/src/main/java/com/example/auth/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("test");
+    }
+}
